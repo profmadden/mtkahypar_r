4234ebe774ddaea3041833f62facbba03e19d57b
Patrick H Madden <pmadden@acm.org>
2025-03-08 22:40:29 -0500

    Did a format, and updated the README a bit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# kahypar_r
-KaHyPar wrapper library for Rust
+# mtkahypar_r
+mt-KaHyPar wrapper library for Rust
 
 --
 
-Install kahypar.ini somewhere readable on the machine; the partitioner needs to be able to find this file and load it in for a partitioning context.  Essentially, a configuration file.
+Install mtkahypar.ini somewhere readable on the machine; the partitioner needs to be able to find this file and load it in for a partitioning context.  Essentially, a configuration file.
 
 The KaHyPar library also needs to be installed.  Download and build from [kaypar.org](https://kahypar.org)
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::os::raw::{c_int, c_uint, c_ulong, c_char};
+use std::os::raw::{c_char, c_int, c_uint, c_ulong};
 
 use cty::c_float;
 
@@ -10,9 +10,20 @@ mod ffi {
 unsafe extern "C" {
     pub fn mtkahypar_hello();
     pub fn mtkahypar_test_partitioner(filename: *const u8);
-    pub fn mtkahypar_partition(nvtxs: c_uint, nhedges: c_uint, hewt: *const c_int, vtw: *const c_int, eind: *const c_ulong, eptr: *const c_ulong, part: *mut c_int, kway: c_int, passes: c_int, seed: c_ulong, imbalance: c_float);
+    pub fn mtkahypar_partition(
+        nvtxs: c_uint,
+        nhedges: c_uint,
+        hewt: *const c_int,
+        vtw: *const c_int,
+        eind: *const c_ulong,
+        eptr: *const c_ulong,
+        part: *mut c_int,
+        kway: c_int,
+        passes: c_int,
+        seed: c_ulong,
+        imbalance: c_float,
+    );
 }
-
 
 pub fn mtkahypar_test(data: Vec<i32>) {
     println!("Here we are with KaHyPar!");
@@ -20,8 +31,6 @@ pub fn mtkahypar_test(data: Vec<i32>) {
         println!("Data value {}", v);
     }
     unsafe {
-	  mtkahypar_hello();
+        mtkahypar_hello();
     }
 }
-
-
